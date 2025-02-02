75923f66de64d4e547def339692736f2f1489b8e
Anvith S G <anvithsg2004@gmail.com>
2025-02-02 14:59:33 +0530

    Started Frontend

---
+++ rca-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}",  // Ensure HTML files in src are included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

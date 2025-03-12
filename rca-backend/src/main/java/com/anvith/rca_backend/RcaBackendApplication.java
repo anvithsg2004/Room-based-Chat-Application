08204c3e3e7f5b5f7adedfa84e69416f2923afc8
Anvith S G <anvithsg2004@gmail.com>
2025-03-12 12:51:08 +0530

    Added Spring Boot Acuator

--- rca-backend/src/main/java/com/anvith/rca_backend/RcaBackendApplication.java
+++ rca-backend/src/main/java/com/anvith/rca_backend/RcaBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RcaBackendApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(RcaBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RcaBackendApplication.class, args);
+    }
 }
